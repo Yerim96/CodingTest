caf549a6ec9df5ef9adaa83eda4db564baa34af7
Yerim96 <80906567+Yerim96@users.noreply.github.com>
2022-09-17 12:51:26 +0900

    짝수와 홀수

---
+++ EvenAndOdd/EvenAndOdd.java
@@ -0,0 +1,14 @@
+package CodingTest;
+
+import fsafaf.CodingTest;
+
+public class EvenAndOdd {
+    public static void main(String[] args) {
+        int num=3;
+        if(num%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
